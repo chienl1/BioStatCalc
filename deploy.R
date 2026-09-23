1eff2435117cb66fbedffeeafd208781228fe892
Lung-Chang Chien <chienl1@ad.unlv.edu>
2026-09-23 11:39:36 -0700

    Initial commit for Shiny app

--- deploy.R
+++
@@ -1,8 +0,0 @@
-install.packages('rsconnect')
-rsconnect::setAccountInfo(name='biostatcalc', token='D78F5C2160CB78EFCB5F0E4AB6B4F978', secret='dI7zyigUWGeIgy5ie0yitnUSxL/KQE9sdvndPBLi')
-library(rsconnect)
-rsconnect::deployApp(
-  appDir = 'C:/Users/chienl1/Dropbox/course/2026/EAB703-FA/R Shinny App',
-  appPrimaryDoc = 'BioStatCalc.R',
-  appMode = 'shiny'
-)
